518c1e092bfd704b5c243d6e3627dedbca7e81d4
Jair-F <jair.fehlauer@gmail.com>
2022-01-09 12:16:03 +0200

    day6 - part 1 and 2

---
+++ 2021/day 6/part1 - 2.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+os.system("clear")
+
+
+input_data = str()
+with open("part1 - input.txt") as input:
+    input_data += input.readline()
+latern_fish = input_data.split(',')
+
+for i in range(len(latern_fish)):
+    latern_fish[i] = int(latern_fish[i])
+
+print(latern_fish)
+
+
+for i in range(256): # number of days part 1: 80 part2: 256(takes a lot of memory and time!!)
+    fish_to_add = list()
+    for i in range(len(latern_fish)):
+        if latern_fish[i] == 0:
+            fish_to_add.append(8)
+            latern_fish[i] = 6
+        else :
+            latern_fish[i] -= 1
+    for x in fish_to_add:
+        latern_fish.append(x)
+    #print(latern_fish)
+
+print(len(latern_fish))
